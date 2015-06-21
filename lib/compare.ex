44f5b41c8e5ce3287b9aa272550126efce52c30d
Arthur Colle <hello@arthurcolle.com>
2015-06-21 18:01:52 -0400

    Change lat/lng keys to atoms and change String to Float

--- lib/compare.ex
+++ lib/compare.ex
@@ -1,9 +1,9 @@
 defmodule Compare do
   def distance(p1, p2) do
-    lat1 = String.to_float p1["latitude"]
-    lon1 = String.to_float p1["longitude"]
-    lat2 = String.to_float p2["latitude"]
-    lon2 = String.to_float p2["longitude"]
+    lat1 = p1[:latitude]
+    lon1 = p1[:longitude]
+    lat2 = p2[:latitude]
+    lon2 = p2[:longitude]
     radlat1 = :math.pi * lat1/180
     radlat2 = :math.pi * lat2/180
     radlon1 = :math.pi * lon1/180
@@ -18,4 +18,7 @@ defmodule Compare do
   end
 end
 
-# Compare.distance(%{"latitude" => "39.0562619, "longitude" => -77.13000090000003},%{"latitude" => 38.9909652, "longitude" => -76.92953649999998})
+# Compare.distance(
+#	%{"latitude" => user1.latitude, "longitude" => user1.longitude}, 
+#	%{"latitude" => user2.latitude, "longitude" => user2.longitude}
+# )
